aeae965e7b083cb643561c8c5b1f2a42a110acd3
Sahil Raj <work.sahilraj@gmail.com>
2025-03-17 23:00:18 +0530

    add them

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    colors: {
+      gray: "#1F2937",
+      white: "#FFFFFF",
+      blue: "#0000FF",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
